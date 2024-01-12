b0084fba2ef696b4f450a16e33ba4bd7da96c483
Anatoliy Samoylov <a9295730700@yandex.ru>
2024-01-12 20:20:25 +0300

    P2C-000-project-migration

---
+++ src/main/java/ru/avsamoylov/painmarket1/PainMarket1Application.java
@@ -0,0 +1,13 @@
+package ru.avsamoylov.painmarket1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainMarket1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainMarket1Application.class, args);
+	}
+
+}
